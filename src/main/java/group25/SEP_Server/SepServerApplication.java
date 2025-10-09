1bae563f1a18a4f100c798e14168c2c4a8f601ed
vtmhieu <vtmhieu111@gmail.com>
2025-10-09 20:58:48 +0200

    spring initalizer

---
+++ src/main/java/group25/SEP_Server/SepServerApplication.java
@@ -0,0 +1,13 @@
+package group25.SEP_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group25/SEP_Server/SepServerApplicationTests.java
@@ -0,0 +1,13 @@
+package group25.SEP_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
