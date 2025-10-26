df38db8116059d2442a514fb2d8997eea4d8801a
Thilini <kthiliniyashodha@gmail.com>
2025-10-26 14:07:32 +0100

    Event model modification

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO EVENT (name, location, event_date, type, description, estimate_budget, status) VALUES ('Tech Summit 2025', 'Negombo', '2025-12-15', 'type1', 'Annual technology conference', '100.0', 'PENDING');
+INSERT INTO EVENT (record_id, name, location, event_date, type, description, estimate_budget, status, attendees, start_date, end_date) VALUES ( 'EV-1', 'Tech Summit 2025', 'Negombo', '2025-12-15', 'type1', 'Annual technology conference', '100.0', 'PENDING', 10, '2025-10-28', '2025-10-30');
 
-INSERT INTO EVENT (name, location, event_date, type, description, estimate_budget, status) VALUES ('AI Expo', 'Colombo', '2025-11-20', 'type1', 'AI and Data Science Exhibition', '200.0', 'PENDING');
+INSERT INTO EVENT (record_id, name, location, event_date, type, description, estimate_budget, status, attendees, start_date, end_date) VALUES ('EV-2', 'AI Expo', 'Colombo', '2025-11-20', 'type1', 'AI and Data Science Exhibition', '200.0', 'PENDING', 10, '2025-10-28', '2025-10-30');
 
-INSERT INTO EVENT (name, location, event_date, type, description, estimate_budget, status) VALUES ('Developer Meetup', 'Kandy', '2025-10-30', 'type1', 'Regional developer community meetup', '130.0', 'ACCEPTED');
+INSERT INTO EVENT (record_id, name, location, event_date, type, description, estimate_budget, status, attendees, start_date, end_date) VALUES ('EV-3', 'Developer Meetup', 'Kandy', '2025-10-30', 'type1', 'Regional developer community meetup', '130.0', 'ACCEPTED', 10, '2025-10-28', '2025-10-30');
