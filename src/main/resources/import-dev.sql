b1ba905cccee9f5a50cd1ef802dc0fafd72f2db8
Tpopescu08 <104749489+Tpopescu08@users.noreply.github.com>
2025-10-29 13:35:18 +0100

    change added task to be music subteam

--- src/main/resources/import-dev.sql
+++ src/main/resources/import-dev.sql
@@ -9,7 +9,7 @@ INSERT INTO TASK (event_id, project_reference, description, assignee, subteam, c
 
 INSERT INTO TASK (event_id, project_reference, description, assignee, subteam, comments, priority, status) VALUES ('1', 'Tech Summit 2025', 'Be able to set up around 10 tables for seating during the lunch break', 'Adam', 'Food', 'Only have 6 tables currently in stock, will need to purchase more', 'MEDIUM', 'NEEDS_MANAGER_REVIEW');
 
-INSERT INTO TASK (event_id, project_reference, description, assignee, subteam, comments, priority, status) VALUES ('2', 'AI Expo', 'Set up the lighting/sound for all the booths at the expo', 'Sarah', 'Design', '', 'CRITICAL', 'CREATED');
+INSERT INTO TASK (event_id, project_reference, description, assignee, subteam, comments, priority, status) VALUES ('2', 'AI Expo', 'Set up the lighting/sound for all the booths at the expo', 'Sarah', 'Music', '', 'CRITICAL', 'CREATED');
 
 INSERT INTO TASK (event_id, project_reference, description, assignee, subteam, comments, priority, status) VALUES ('3', 'Developer Meetup', 'Have some small snacks on standby for developers who attend the meet', 'Brock', 'Food', 'Small task, easily doable', 'LOW', 'IN_PROGRESS');
 
